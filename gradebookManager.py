aa2c3136196cb8ca71c88fb0e787aed67c77092e
NoahBradberry <nbradberry425@phm.k12.in.us>
2025-09-12 09:47:16 -0400

    Update gradebookManager.py

--- gradebookManager.py
+++ gradebookManager.py
@@ -1,19 +1,33 @@
-names = ["Kyle", "Ed", "Stuart"]
-Kyle_grades = [90, 83, 70]
-Ed_grades = [80, 80, 75]
-Stuart_grades = [98, 70, 98]
-gradebook = {"Kyle" : Kyle_grades, "Ed" : Ed_grades, "Stuart" : Stuart_grades}
-
 def average(student):
     total = 0
     for l in student:
         total += l
     average = total / len(student)
-    return average
+    return int(average)
+
+def letter(grade):
+    if grade >= 90:
+        return("A")
+    elif grade >= 80:
+        return("B")
+    elif grade >= 70:
+        return("C")
+    elif grade >= 60:
+        return("D")
+    else:
+        return("F")
+    
+
+names = ["Kyle", "Ed", "Stuart"]
+Kyle_grades = [90, 83, 70]
+Ed_grades = [80, 80, 75]
+Stuart_grades = [98, 70, 98]
+gradebook = {"Kyle" : Kyle_grades, "Ed" : Ed_grades, "Stuart" : Stuart_grades}
+averages = {"Kyle" : average(Kyle_grades), "Ed" : average(Ed_grades), "Stuart" : average(Stuart_grades)}
 
 
 while True:
-    action = input("Add student, Add grades, View gradebook, end ")
+    action = input("Add student, Add grades, View gradebook, Remove student, Remove grade, end ")
     action = action.upper()
     if action == "ADD STUDENT":
         name = input("What is the student's name? ")
@@ -25,10 +39,48 @@ while True:
             grade = int(input("Enter Grade: "))
             grades.append(grade)
         gradebook[name] = grades
+        averages[name] = average(grades)
+
+    elif action == "ADD GRADES" :
+        student = input("What student would you like to add a grade to? ")
+        student = student.capitalize()
+        if student in names:
+            grade = int(input("Input Grade: "))
+            gradebook[student].append(grade)
+            averages [student] = average(gradebook[student])
+            break
+        else:
+            print("Name not in gradebook, please try again")
+
 
     elif action == "VIEW GRADEBOOK":
-        for l in names:
-            print(l + ": " + str(gradebook[l]) + " Average: " + str(int(average(gradebook[l]))))
+        averages = dict(sorted(averages.items(), key = lambda item: item[1]))
+        averages = dict(reversed(list(averages.items())))
+        for l in averages:
+            print(l + ":" + str(gradebook[l]) + " Average: " + str(average(gradebook[l])) + " " + letter(average(gradebook[l])))
+        print("Top Student: " + max(averages))
+
+    elif action == "REMOVE STUDENT":
+        student = input("What student would you like to remove? ")
+        student = student.capitalize()
+        if student in names:
+            names.remove(student)
+            del gradebook[student]
+            del averages[student]
+        else:
+            print("Student not in gradebook, please try again.")
+    elif action == "REMOVE GRADE":
+        student = input("What student would you like to remove a grade from? ")
+        student = student.capitalize()
+        if student in names:
+            print(student + "'s Grades: " + str(gradebook[student]))
+            grade_to_remove = int(input("Which grade would you like to remove"))
+            if grade_to_remove in gradebook[student]:
+                gradebook[student].remove(grade_to_remove)
+            else:
+                print("Not one of student's grades please try again")
+        else:
+            print("Name not in gradebook, please try again")
 
 
     elif action == "END":
