0265e5ad2805439f29ec65a22e239e3aafe23234
NoahBradberry <nbradberry425@phm.k12.in.us>
2025-09-24 09:51:08 -0400

    pandas

--- pandasFunStuff.py
+++ pandasFunStuff.py
@@ -5,10 +5,11 @@ bfreq = [183330, 173981, 163266, 159945, 157875, 149082, 145171, 142142, 139652,
 gfreq = [195028, 184528, 181132, 170559, 155844, 129088, 118713, 117626, 102470, 98419]
 df = pd.DataFrame(
     {
-        "Boys Names" : bnames,
-        "Boys Frequency" : bfreq,
-        "Girls Names" : gnames,
-        "Girls Frequency" : gfreq
+        "Boys_Names" : bnames,
+        "Boys_Frequency" : bfreq,
+        "Girls_Names" : gnames,
+        "Girls_Frequency" : gfreq
     }
 )
 print(df)
+print(round(df.describe()))
---
+++ pennData.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import random
+
+fNames = ["Dave", "Mack", "Daniel", "Aiden", "Ben", "Joey", "Christina", "Noah", "Matthew", "Markus", "Jordan", "Josh", "Patrick", "Stephanie", "Jill", "Jane", "Emma", "Catherine", "Rachel", "Marge", "Lisa"]
+lNames = ["Smith", "Jones", "Johnson", "Lewis", "Brown", "Garcia", "Davis", "Miller", "Savoie", "Anderson", "Rulli", "Bradberry", "Patel", "Robinson", "Jordan"]
+years = ["Freshmen", "Sophomore", "Junior", "Senior", "Victory Lap"]
+pathways = ["Early College", "Engineering", "Computer Science", "Business", "Marketing", "Early Childhood Education", "Culinary", "Criminal Justice", "Bio Med"]
+names = []
+
+for i in range(20000):
+    names.append(f"{random.choice(fNames)} {random.choice(lNames)}")
+
+data = {
+    "Name" : names,
+    "Age": [random.randint(14, 19) for _ in names],
+    "GPA" : [round(random.uniform(0.3, 4.3),2) for _ in names],
+    "Credits Completed" : [random.randint(0,60) for _ in names],
+    "Year" : [random.choice(years) for _ in names],
+    "Pathway" : [random.choice(pathways) for _ in names]
+}
+
+pennData = pd.DataFrame(data)
+print(pennData)
+pennData.to_csv("pennData.csv")
